a7d98384edb12b7d23504858360124fa4b8c5bcd
leongst9 <30487410+leongst9@users.noreply.github.com>
2018-08-19 01:11:13 +0800

    Create mytesting.py

---
+++ mytesting.py
@@ -0,0 +1,46 @@
+import telepot
+import RPi.GPIO as GPIO
+import sys
+import time
+
+#Lights control
+def on(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        return
+def off(pin):
+        GPIO.output(pin,GPIO.LOW)
+        return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# set up GPIO output channel
+GPIO.setup(11, GPIO.OUT)
+
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print('Got command: %s' % command)
+
+    if command == 'on':
+       bot.sendMessage(chat_id, on(11))
+    elif command =='off':
+       bot.sendMessage(chat_id, off(11))
+
+bot = telepot.Bot(559250359:AAHKhRrbjf237BeZPxrdAl4CZGH02D_aDcI)
+MessageLoop(bot, handle).run_as_thread()
+print('I am listening...')
+
+while 1:
+    try:
+        time.sleep(10)
+    
+    except KeyboardInterrupt:
+        print('\n Program interrupted')
+        GPIO.cleanup()
+        exit()
+    
+    except:
+        print('Other error or exception occured!')
+        GPIO.cleanup()
